305204f744e906f1ace62ee7ccbdff9c44483cbd
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-06-20 00:02:53 +0700

    feat(QP-18): setup store using useReducer

---
+++ src/store/reducer.js
@@ -0,0 +1,8 @@
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        default:
+            console.error(`Unhandled action type: ${action.type}`)
+            return state
+    }
+}
