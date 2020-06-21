5d6926bbdd5c005406cba4c353028dd86155a98d
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-06-21 14:46:59 +0700

    feat(QP-18): add FullScreen layout

---
+++ src/components/layouts/FullScreenLayout/FullScreenLayout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FullScreenLayout({children}) {
+
+    return (
+        <div className="full-screen">
+            {children}
+        </div>
+    )
+}
+
+export {FullScreenLayout}
