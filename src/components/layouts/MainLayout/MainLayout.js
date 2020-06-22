ebaae0db9ac871ec2ab7164241c0939987ece71c
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-06-22 14:23:33 +0700

    feat(QP-10): add header and navbar

--- src/components/layouts/MainLayout/MainLayout.js
+++ src/components/layouts/MainLayout/MainLayout.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { HeaderV2 } from '../..'
 
 function MainLayout({children}) {
 
     return (
         <div className="main-layout">
+            <HeaderV2 title="QR team" />
             {children}
         </div>
     )
