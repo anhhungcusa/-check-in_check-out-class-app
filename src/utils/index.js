2daaee55b9cb7dbd2aa90162b043310c37ece8cb
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-06-23 19:47:35 +0700

    fix(QP-39): can't decode token that is not jwt

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,19 @@
+import decode from 'jwt-decode'
 function Exception(message, status) {
     this.message  = message
     this.status = status
 }
 
 
+function jwtDecode(token) {
+    try {
+        if(!token) throw new Error('token not found')
+        return decode(token)
+    } catch (error) {
+        return false
+    }
+}
 
-export {Exception}
+
+
+export {Exception, jwtDecode}
